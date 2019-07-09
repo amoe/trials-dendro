236b14e8ed97f4a613c22cab576dc6f40cb6fe71
David Banks <amoebae@gmail.com>
2019-07-09 10:56:10 +0100

    add tree data

---
+++ sample_tree_data.js
@@ -0,0 +1,26 @@
+const ALL_TRIALS = {
+    name: "All Trials",
+    sentenceCount: 57804,
+    children: [
+        {
+            name: "Verdict/Guilty",
+            sentenceCount: 35260,
+            children: [
+                {
+                    name: "Illuminated",
+                    sentenceCount: 449
+                }
+            ]
+        },
+        {
+            name: "Verdict/Not Guilty",
+            sentenceCount: 17000,
+            children: [
+                {
+                    name: "Illuminated",
+                    sentenceCount: 463
+                }
+            ]
+        }
+    ]
+};
